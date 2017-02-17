771ec653a32b4b37437a6d4be0c3c72f35bd6fd5
Oleksii Liashuk <oleksii_liashuk@epam.com>
2017-02-17 10:51:52 +0200

    Tuned consul_pinger script

--- ansible/roles/nginx/files/consul_pinger.sh
+++ ansible/roles/nginx/files/consul_pinger.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-nc -dk -l 65432 & prev_pid=$!
-echo ${prev_pid}
-nc -z localhost 65432 || (consul-template -consul localhost:8500 -template "/data/nginx/consul_templ/app.templ:/data/nginx/conf.d/app_upstream.conf:docker exec NginxLatest service nginx reload" > /tmp/consul-template.log; kill -9 ${prev_pid})
+set +e
+
+nc -dk -l 65432 &
+consul-template -consul localhost:8500 -template "/data/nginx/consul_templ/app.templ:/data/nginx/conf.d/app_upstream.conf:docker exec NginxLatest service nginx reload" > /tmp/consul-template.log
+echo Killing: $$
+kill -9 -$$

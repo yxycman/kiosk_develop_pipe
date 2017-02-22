e0732ada040c12772d62a4a7a57c2305cc32be70
Oleksii Liashuk <oleksii_liashuk@epam.com>
2017-02-22 13:28:53 +0200

    Exit, if netcat already runnig

--- ansible/roles/nginx/files/consul_pinger.sh
+++ ansible/roles/nginx/files/consul_pinger.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set +e
 
+nc -w 1 localhost 65432 && exit 100
 nc -dk -l 65432 &
+ncPid=$!
 consul-template -consul localhost:8500 -template "/data/nginx/consul_templ/app.templ:/data/nginx/conf.d/app_upstream.conf:docker exec NginxLatest service nginx reload" > /tmp/consul-template.log
-echo Killing: $$
-kill -9 -$$
+kill -9 ${ncPid}
